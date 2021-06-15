c599fb33c098a2a0735d22cd99ae4b0e31a9f4b8
Mauro <72555322+Mauro1986@users.noreply.github.com>
2021-06-15 12:03:51 +0200

    .

---
+++ MvcCoreOverview2/Controllers/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcCoreOverview2.Models;
+using MVCEFCoreOverview.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreOverview2.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly BookContext _db;
+        public AuthorController(BookContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Authors> authors = _db.Authors.ToList();
+            return View(authors);
+        }
+    }
+}
---
+++ MvcCoreOverview2/Migrations/20210615093002_SeedDataForAutors.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcCoreOverview2.Migrations
+{
+    public partial class SeedDataForAutors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Authors values('Jan', 'Jambon', 'Brussel')");
+            migrationBuilder.Sql("insert into Authors values('Kenan', 'Kurda', 'Gent')");
+            migrationBuilder.Sql("insert into Authors values('Emre', 'Elagoz', 'Antwerpen')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
