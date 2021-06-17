b4a0b0e886503cc00bc27e57bd4b89cd3439c5f4
Mauro <72555322+Mauro1986@users.noreply.github.com>
2021-06-17 10:36:51 +0200

    .

--- MvcCoreOverview2/Controllers/AuthorController.cs
+++ MvcCoreOverview2/Controllers/AuthorController.cs
@@ -18,8 +18,34 @@ namespace MvcCoreOverview2.Controllers
         }
         public IActionResult Index()
         {
-            List<Authors> authors = _db.Authors.ToList();
+            //List<Authors> authors = _db.Authors.ToList();     // Kan je authors.add doen
+            IEnumerable<Authors> authors = _db.Authors.ToList(); //kan ook maar geen .add
+            
             return View(authors);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]  //VEILIGHEID!  maakt key zodat request enkel van deze app gebruikt worden
+        public async Task<IActionResult> Create(Authors author)
+        {
+            if (ModelState.IsValid) //(controle van alle velden of correct ingevuld)
+            {
+                //_db.Authors.Add(author); 
+                //_db.SaveChanges();
+
+                //_db.Authors.AddAsync(author);
+
+                _db.Authors.Add(author);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index)); // nameof helpt fout vermijden
+            }
+            return View();
         }
     }
 }
